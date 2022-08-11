e7b363c7f0bbc70ac2cf7e2cd47d3cc79077a893
José Manuel Cernuda <jlcu@gft.com>
2022-08-11 16:32:07 +0200

    Deleting api_key for security

--- app/src/main/java/com/josecernu/weatherforecast/utils/Constants.kt
+++ app/src/main/java/com/josecernu/weatherforecast/utils/Constants.kt
@@ -2,5 +2,5 @@ package com.josecernu.weatherforecast.utils
 
 object Constants {
     const val BASE_URL = "https://api.openweathermap.org/"
-    const val API_KEY = "cb69bac5a79e80c292ca55428d59f764"
+    const val API_KEY = "ADD_YOUR_OWN_APP_ID_HERE"
 }
