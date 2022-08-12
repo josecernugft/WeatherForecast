0428202bb120193926a34106eb1118b1d682b710
José Manuel Cernuda <jlcu@gft.com>
2022-08-12 10:57:39 +0200

    Improving MainScreen

--- app/src/main/java/com/josecernu/weatherforecast/screens/main/MainScreen.kt
+++ app/src/main/java/com/josecernu/weatherforecast/screens/main/MainScreen.kt
@@ -1,19 +1,39 @@
 package com.josecernu.weatherforecast.screens.main
 
 import android.util.Log
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
+import com.josecernu.weatherforecast.R
 import com.josecernu.weatherforecast.data.DataOrException
 import com.josecernu.weatherforecast.model.Weather
-import com.josecernu.weatherforecast.widgets.WeatherAppBar
+import com.josecernu.weatherforecast.model.WeatherItem
+import com.josecernu.weatherforecast.utils.formatDate
+import com.josecernu.weatherforecast.utils.formatDateTime
+import com.josecernu.weatherforecast.utils.formatDecimals
+import com.josecernu.weatherforecast.widgets.*
 
 @Composable
 fun MainScreen(navController: NavController,
@@ -47,5 +67,57 @@ fun MainScaffold(weather: Weather, navController: NavController) {
 
 @Composable
 fun MainContent(data: Weather) {
-    Text(text = data.city.name)
+    val weatherItem = data.list[0]
+    val imageUrl = "https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}.png"
+    Column(
+        Modifier
+            .padding(4.dp)
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Text(text = formatDate(weatherItem.dt), // Wed Nov 30
+        style = MaterialTheme.typography.caption,
+        color = MaterialTheme.colors.onSecondary,
+        fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(6.dp))
+        Surface(modifier = Modifier
+            .padding(4.dp)
+            .size(200.dp),
+            shape = CircleShape,
+            color = Color(0xFFFFC400)) {
+
+            Column(verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                WeatherStateImage(imageUrl = imageUrl)
+                Text(text = formatDecimals(weatherItem.main.temp) + "º",
+                    style = MaterialTheme.typography.h4,
+                    fontWeight = FontWeight.ExtraBold)
+                Text(text = weatherItem.weather[0].main, fontStyle = FontStyle.Italic)
+
+            }
+        }
+        HumidityWindPressureRow(weather = data.list[0])
+        Divider()
+        SunsetSunRiseRow(weather = data)
+        Text("Next days",
+            style = MaterialTheme.typography.subtitle1,
+            fontWeight = FontWeight.Bold
+        )
+        Surface(modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        color = Color(0xFFEEF1EF),
+        shape = RoundedCornerShape(size = 14.dp)
+        ) {
+            LazyColumn(modifier = Modifier.padding(2.dp),
+                contentPadding = PaddingValues(1.dp)) {
+                items(items = data.list) {
+                    item: WeatherItem ->
+                    WeatherDetailRow(weather = item)
+                }
+            }
+
+        }
+    }
 }
+
--- app/src/main/java/com/josecernu/weatherforecast/utils/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.josecernu.weatherforecast.utils
-
-object Constants {
-    const val BASE_URL = "https://api.openweathermap.org/"
-    const val API_KEY = "ADD_YOUR_OWN_APP_ID_HERE"
-}
