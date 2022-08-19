b06b160a62676d71f7cc0f0628b590460ab118a1
José Manuel Cernuda <jlcu@gft.com>
2022-08-19 17:16:13 +0200

    Adding api key to gradle.properties

---
+++ app/src/main/java/com/josecernu/weatherforecast/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.josecernu.weatherforecast.utils
+
+import com.josecernu.weatherforecast.BuildConfig
+
+object Constants {
+    const val BASE_URL = "https://api.openweathermap.org/"
+    const val API_KEY = BuildConfig.API_KEY
+}
